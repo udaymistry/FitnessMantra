4a369f44d3ce35144354e1452410d2fea622ff5e
udaymistry <coolboy1332@gmail.com>
2016-01-19 20:43:22 +0530

    Updated! APP!

--- app/src/main/java/com/example/sonal/fitnessmantra/MainActivity.java
+++ app/src/main/java/com/example/sonal/fitnessmantra/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.sonal.fitnessmantra;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+  }
 
+    public void onClick(View view){
+
+        Intent o=new Intent(this,BmiCal.class);
+        startActivity(o);
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
