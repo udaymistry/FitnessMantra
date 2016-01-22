6c1dd57c0e3298b1fa58c594133f33eb212d3acb
udaymistry <coolboy1332@gmail.com>
2016-01-22 21:44:12 +0530

    Updated! APP!

--- app/src/main/java/com/example/sonal/fitnessmantra/Exercise.java
+++ app/src/main/java/com/example/sonal/fitnessmantra/Exercise.java
@@ -1,31 +1,22 @@
 package com.example.sonal.fitnessmantra;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class Exercise extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-  }
+        setContentView(R.layout.activity_exercise);
+    }
 
-    public void onClick(View view){
-
-        Intent i=new Intent(this,Bmi_calcy.class);
-        startActivity(i);
-            }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_exercise, menu);
         return true;
     }
 
